a3a77050f46fae7ba9dbf064c06f595a58b23a28
Cameron Hilton <cameronhilton@gmail.com>
2020-05-19 10:20:27 -0700

    create tab/stack navigation to App

--- App.js
+++ App.js
@@ -1,26 +1,104 @@
+import 'react-native-gesture-handler'
 import React from 'react'
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import Constants from 'expo-constants'
-import Deck from './components/Deck'
 import DeckList from './components/DeckList'
 import Quiz from './components/Quiz'
+import { lightBlue, red, white } from './utils/colors'
+
+const RouteConfigs = {
+  DeckList: {
+    name: 'DeckList',
+    component: DeckList,
+    options: {
+      tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />,
+      title: 'All Decks'
+    }
+  }, 
+  Quiz: {
+    component: Quiz,
+    name: 'Quiz',
+    options: {
+      tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />,
+      title: 'Quiz'
+    }
+  },
+}
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? lightBlue : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : lightBlue,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    }
+  }
+}
+
+const Tab = Platform.OS === 'ios'
+  ? createBottomTabNavigator() 
+  : createMaterialTopTabNavigator()
+
+const TabNav = () => (
+  <Tab.Navigator {...TabNavigatorConfig}>
+    <Tab.Screen {...RouteConfigs['DeckList']} />
+    <Tab.Screen {...RouteConfigs['Quiz']} />
+  </Tab.Navigator>
+)
+
+const StackNavigatorConfig = {
+  headerMode: 'screen',
+}
+
+const StackConfig = {
+  TabNav: {
+    name: 'Home',
+    component: TabNav,
+    options: { headerShown: false },
+  }, 
+  DeckList: {
+    name: 'DeckList',
+    component: DeckList,
+    options: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: lightBlue,
+      },
+      title: 'DeckList',
+    },
+  }
+}
+
+const Stack = createStackNavigator();
+
+const MainNav = () => (
+  <Stack.Navigator {...StackNavigatorConfig}>
+    <Stack.Screen {...StackConfig['TabNav']}/>
+    <Stack.Screen {...StackConfig['DeckList']}/>
+  </Stack.Navigator>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Main</Text>
-      <Deck/>
-      <DeckList/>
-      <Quiz/>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <NavigationContainer>
+        <MainNav/>
+      </NavigationContainer>
+    </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
