52631ffee910c617ffc04429c9d76989ae916520
Cameron Hilton <cameronhilton@gmail.com>
2020-05-19 16:07:26 -0700

    add ability to create NewDeck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { connect } from 'react-redux'
 import DeckHeader from './DeckHeader'
+import { addDeck } from '../actions'
+import { saveDeckTitle } from '../utils/helpers'
 
-export default class NewDeck extends Component {
+class NewDeck extends Component {
+  state = {
+    value: '',
+  }
+
+  onChangeText = (text) => this.setState({value: text})
+
+  handleOnPress = () => {
+    const { dispatch } = this.props
+
+    saveDeckTitle(this.state.value)
+      .then((deck) => {
+        dispatch(addDeck(deck))
+      })
+    
+    this.setState(() => ({
+      value: '',
+    }))
+  }
+
   render() {
+    const { value } = this.state
+
     return (
       <View>
         <DeckHeader title='Create New Deck'/>
-        {/* input title, create deck button */}
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          placeholder='New Deck Name'
+          value={value}
+          onChangeText={text => this.onChangeText(text)}
+          maxLength={50}
+        />
+        <TouchableOpacity
+          onPress={this.handleOnPress}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+export default connect()(NewDeck)
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,16 +3,28 @@ import { AsyncStorage } from 'react-native'
 const DECK_STORAGE_KEY = 'FlashCards:decks'
 
 // return all of the decks along with their titles, questions, and answers
-export function getDecks() {
-  return AsyncStorage.getItem(DECK_STORAGE_KEY)
-    .then((results) => {
-      // TODO: remove after testing
-      if (results === null) {
-        addSampleCards()
-      }
+export async function getDecks() {
+  const results = await AsyncStorage.getItem(DECK_STORAGE_KEY)
+  // TODO: remove after testing
+  if (results === null) {
+    addSampleCards()
+  }
 
-      return samples
-    })
+  return samples
+}
+
+// add title to the decks
+export async function saveDeckTitle(title) {
+  const newDeck = {
+    [title]: {
+      title,
+      questions: [],
+    },
+  }
+
+  console.warn(title)
+  await AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(newDeck))
+  return (newDeck)
 }
 
 const samples = {
