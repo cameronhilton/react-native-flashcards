174cdf8d2854d6008fe388361ea27a40270456b6
Cameron Hilton <cameronhilton@gmail.com>
2020-05-19 13:20:40 -0700

    show decks in DeckList view

--- components/Deck.js
+++ components/Deck.js
@@ -4,10 +4,11 @@ import DeckHeader from './DeckHeader'
 
 export default class Deck extends Component {
   render() {
+    const { deck } = this.props
+
     return (
       <View>
-        <DeckHeader title='Some Deck'/>
-        <Text>Deck</Text>
+        <DeckHeader title={deck.title}/>
       </View>
     )
   }
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { AppLoading } from 'expo'
 import Deck from './Deck'
+import DeckHeader from './DeckHeader'
+import { getDecks } from '../utils/helpers'
 
 export default class DeckList extends Component {
+  state = {
+    decks: null,
+    ready: false,
+  }
+
+  componentDidMount() {
+    getDecks()
+      .then((decks) => this.setState({
+        decks,
+        ready: true,
+      }))
+  }
+
   render() {
+    const { decks, ready } = this.state
+
+    if (ready === false) {
+      return <AppLoading/>
+    }
+
     return (
       <View>
-        <Text>DeckList</Text>
-        <Deck/>
+        <DeckHeader title='Deck List'/>
+        {decks === null
+          ? <View>
+              <Text>No decks created yet</Text>
+            </View>
+          : Object.keys(decks).map((deck) => {
+              return (
+                <Deck key={deck} deck={decks[deck]}/>
+              )
+            })}
       </View>
     )
   }
