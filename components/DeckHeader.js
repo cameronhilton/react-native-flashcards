8bf5a8ee6f8a3d6a648dc3c24e3a021602cd436b
Cameron Hilton <cameronhilton@gmail.com>
2020-05-19 21:33:17 -0700

    make DeckList scrollable with card counts

--- components/DeckHeader.js
+++ components/DeckHeader.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Text } from 'react-native'
 import { lightBlue } from '../utils/colors'
 
-export default function DeckHeader({ title }) {
+export default function DeckHeader({ fontSize, title }) {
   return (
-    <Text style={{color: lightBlue, fontSize: 25}}>
+    <Text style={{color: lightBlue, fontSize: fontSize || 25, textAlign: 'center'}}>
       {title}
     </Text>
   )
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { connect } from 'react-redux'
 import { AppLoading } from 'expo'
 import Deck from './Deck'
 import DeckHeader from './DeckHeader'
 import { getDecks } from '../utils/helpers'
 import { receiveDecks } from '../actions'
+import { darkBlue, white } from '../utils/colors'
 
 class DeckList extends Component {
   state = {
@@ -18,7 +19,7 @@ class DeckList extends Component {
     getDecks()
       .then((decks) => {
         dispatch(receiveDecks(decks))
-        
+
         this.setState({
           ready: true,
         })
@@ -34,21 +35,62 @@ class DeckList extends Component {
     }
 
     return (
-      <View>
+      <SafeAreaView style={styles.container}>
         <DeckHeader title='Deck List'/>
-        {decks === null
-          ? <View>
-              <Text>No decks created yet</Text>
-            </View>
-          : Object.keys(decks).map((deck) => {
-              return (
-                <Deck key={deck} deck={decks[deck]}/>
-              )
-            })}
-      </View>
+        <ScrollView >
+          {decks === null
+            ? <View>
+                <Text>No decks created yet</Text>
+              </View>
+            : Object.keys(decks).map((deck) => {
+                return (
+                  <View key={deck} style={styles.deck}>
+                    <DeckHeader title={decks[deck].title} fontSize={20}/>
+                    <View style={styles.center}>
+                      <Text style={{fontSize: 18, paddingTop: 5}}>
+                        {decks[deck].questions.length} cards
+                      </Text>
+                    </View>
+                  </View>
+                )
+              })}
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    margin: 20,
+  },
+  deck: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white,
+    borderColor: darkBlue,
+    borderWidth: 1,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    marginTop: 10,
+    marginBottom: 10,
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    color: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    }
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+})
 
 function mapStateToProps(decks) {
   return {
